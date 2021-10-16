d640f238e0592528f6d3ee5cb438d2c3b59b7075
David Pacassi Torrico <david@eps-labs.com>
2021-10-16 20:50:25 +0200

    Add matching

---
+++ match.py
@@ -0,0 +1,13 @@
+from scipy import spatial
+import numpy as np
+
+test_user_1 = [4672,0,226,0,0,196,0,313,319,0,0,0,323,456,0,0,0,0,0,0,0,0,0,0,0,0,0]
+test_user_2 = [0,0,251,10,0,132,0,330,508,0,238,0,873,182,730,0,0,0,151,0,177,512,0,0,0,0,384]
+test_user_3 = [0,0,446,0,167,0,0,353,370,0,0,0,871,339,0,0,0,0,96,0,185,441,0,0,0,0,720]
+
+test_user = test_user_1
+
+vsm_data = np.genfromtxt('vsm.csv', delimiter=",", skip_header=1, usecols=range(1, 28))
+vsm_tree = spatial.KDTree(vsm_data)
+
+print(vsm_tree.query(test_user))
